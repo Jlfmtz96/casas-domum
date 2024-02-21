e4996884571972a49240417dd79aba05604127f1
joseluis <jlmartinez@comebi.com.mx>
2024-02-21 13:40:52 -0600

    casas domum web update desarrollos

--- src/app/(general)/desarrollos/page.tsx
+++ src/app/(general)/desarrollos/page.tsx
@@ -37,34 +37,34 @@ export default function DesarrollosPage() {
                     <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
                 </div>
                 <div className='h-[600px] bg-neutral-900 overflow-hidden relative group hidden lg:flex'>
-                    <Image src={r2} alt="" className='w-full h-full object-cover group-hover:opacity-50' />
-                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000 text-center text-white z-30'>
+                    <Image src={r2} alt="" className='w-full h-full object-cover opacity-50' />
+                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 text-center text-white z-30'>
                         <Link href="/desarrollos/alia-residencial-qro">
                             <p className='uppercase font-bold text-3xl 2xl:text-4xl text-nowrap'>Alia Residencial</p>
                         </Link>
                         <span className='text-sm uppercase tracking-[0.2em] '>San Luis Potosí, Mx</span>
                     </div>
-                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-75 lg:opacity-0 lg:group-hover:opacity-70 lg:transition-opacity lg:duration-1000 text-center text-white z-10'>
+                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-75 text-center text-white z-10'>
                         <Image src={Alia} alt="" height={150} width={150} />
                     </div>
                     {/* Borde en esquinas opuestas */}
-                    <div className="absolute top-2 left-2 border-l-2 border-t-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
-                    <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
+                    <div className="absolute top-2 left-2 border-l-2 border-t-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100"></div>
+                    <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100"></div>
                 </div>
                 <div className='h-[600px] bg-neutral-900 overflow-hidden relative group hidden lg:flex'>
-                    <Image src={r3} alt="" className='w-full h-full object-cover group-hover:opacity-50' />
-                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000 text-center text-white z-30'>
+                    <Image src={r3} alt="" className='w-full h-full object-cover opacity-50' />
+                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 text-center text-white z-30'>
                         <Link href="/desarrollos/alia-residencial-qro">
                             <p className='uppercase font-bold text-3xl 2xl:text-4xl text-nowrap'>Alia Residencial</p>
                         </Link>
                         <span className='text-sm uppercase tracking-[0.2em] '>Querétaro, Mx</span>
                     </div>
-                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-75 lg:opacity-0 lg:group-hover:opacity-70 lg:transition-opacity lg:duration-1000 text-center text-white z-10'>
+                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-75 text-center text-white z-10'>
                         <Image src={Alia} alt="" height={150} width={150}/>
                     </div>
                     {/* Borde en esquinas opuestas */}
-                    <div className="absolute top-2 left-2 border-l-2 border-t-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
-                    <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
+                    <div className="absolute top-2 left-2 border-l-2 border-t-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100"></div>
+                    <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100"></div>
                 </div>
                 <div className='h-[600px] bg-neutral-900 overflow-hidden relative group lg:col-span-3'>
                     <Image src={cdm} alt="" className='w-full h-full object-cover group-hover:opacity-50' />
@@ -97,19 +97,19 @@ export default function DesarrollosPage() {
                     <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
                 </div>
                 <div className='h-[600px] bg-neutral-900 overflow-hidden relative group hidden lg:flex'>
-                    <Image src={r5} alt="" className='w-full h-full object-cover group-hover:opacity-50' />
-                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000 text-center text-white z-30'>
+                    <Image src={r5} alt="" className='w-full h-full object-cover opacity-50' />
+                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 text-center text-white z-30'>
                         <Link href="/desarrollos/alia-residencial-qro">
                             <p className='uppercase font-bold text-3xl 2xl:text-4xl text-nowrap'>Casas Domum</p>
                         </Link>
                         <span className='text-sm uppercase tracking-[0.2em] '>San Luis Potosí, Mx</span>
                     </div>
-                    <div className='absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 opacity-75 lg:opacity-0 lg:group-hover:opacity-70 lg:transition-opacity lg:duration-1000 text-center text-white z-10'>
+                    <div className='absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 opacity-75 text-center text-white z-10'>
                         <Image src={Domum} alt="" height={150} width={150} />
                     </div>
                     {/* Borde en esquinas opuestas */}
-                    <div className="absolute top-2 left-2 border-l-2 border-t-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
-                    <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 lg:transition-opacity lg:duration-1000"></div>
+                    <div className="absolute top-2 left-2 border-l-2 border-t-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100"></div>
+                    <div className="absolute bottom-2 right-2 border-r-2 border-b-2 h-20 w-20 lg:h-52 lg:w-44 opacity-100"></div>
                 </div>
             </div>
         </div>
