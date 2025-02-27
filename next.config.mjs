4d0ad4dac2e40a7a4a5db782bfffc96419059867
Astronet <69477419+Jlfmtz96@users.noreply.github.com>
2025-02-27 11:04:10 -0600

    file url to path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 import path from 'path'; // Importa el módulo path
+import { fileURLToPath } from 'url';
+
+// Obtén el equivalente de __dirname en un módulo ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
     images: {
